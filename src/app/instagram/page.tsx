8d722da1dcca838a7465a8b4fba1312ebc44cf33
Ajay Kumar Nelavagilu Udaya Kumar <n.uajaykumar@gmail.com>
2025-04-24 23:06:46 +0000

    revert back to three in a row

--- src/app/instagram/page.tsx
+++ src/app/instagram/page.tsx
@@ -45,7 +45,7 @@ export default function InstagramPage() {
           </div>
           <div>
             <section className="mb-8">
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {items.map((item, index) => (
                   <div key={index}>
                     {item.type === 'reel' ? (
@@ -63,3 +63,4 @@ export default function InstagramPage() {
     </div>
   );
 }
+
